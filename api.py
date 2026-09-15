6f349bf0cf18903d36475718c85cf89318866dd8
nazin <nazin.nazeri@gmail.com>
2026-09-15 20:35:31 +0000

    set up api calls

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from analytics import fetch_instruments_data
+from analytics import *
 
 app = FastAPI()
 
@@ -11,8 +11,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+#http://localhost:5000/analytics/transactions/3
 
-@app.get("/analytics/instruments")
+@app.get("/analytics/instruments/")
 def get_instruments():
     try:
         data = fetch_instruments_data()
@@ -23,6 +24,20 @@ def get_instruments():
             }
         else:
             raise HTTPException(status_code=404, detail="Instruments not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/analytics/transactions/{client_id}")
+def get_transactions(client_id: int):
+    try:
+        data = fetch_user_transactions(client_id)
+        if data:
+            return {
+                "status": "success",
+                "data": data
+            }
+        else:
+            raise HTTPException(status_code=404, detail="Transactions not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
   
